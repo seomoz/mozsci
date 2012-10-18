d4ce25a0a5980297a4d67fff7f2b0ce685be07e3
Jerry Feng <jerry@seomoz.org>
2012-10-18 13:37:15 -0700

    Added load_model, save_model to Pybrain wrapper

--- test/test_pybrain_wrapper.py
+++ test/test_pybrain_wrapper.py
@@ -2,11 +2,12 @@
 import unittest
 from mozsci.models.pybrain_wrapper import PyBrainNN
 import numpy as np
+import json
 
 class Testpybrain_wrapper(unittest.TestCase):
 
-    def test_xor(self):
-        """Test learning XOR with a neural net"""
+    def train_xor(self):
+        """Trains XOR with a neural net"""
         X = np.array([[0.0, 0.0],
                [0, 1],
                [1, 0],
@@ -15,13 +16,42 @@ class Testpybrain_wrapper(unittest.TestCase):
         net = PyBrainNN(learning_rate=0.1, maxiterations=10000, lam=0.0, args=(2, 3, 1), kwargs={'fast':True, 'bias':True})
         net.fit(X, y)
 
+        return net, X, y
+
+
+    def test_xor(self):
+        net, X, y = self.train_xor()
+
         ypred = net.predict(X)
         ypred_int = (ypred > 0.5).astype(np.int)
+
         self.assertTrue(np.allclose(ypred_int, y))
+
+    def test_load_save_model(self):
+        import tempfile
+
+        net, X, y = self.train_xor()
+
+        # save the model
+        tfile = tempfile.TemporaryFile()
+        net.save_model(tfile)
+
+        # load in the model
+        tfile.seek(0)
+
+        model = json.load(tfile)
+
+        netloaded = PyBrainNN.load_model(model)
+
+        self.assertTrue(np.allclose(net.predict(X), netloaded.predict(X)))
+
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
 
 
 
